a2cec7ea97f34ca78eb28d4ea84d66e3e632a64c
BinkyWong <andy.wong.rpt@gmail.com>
2025-04-21 13:00:40 +0100

    now works in batches of 1000 so gigantic bulk inserts will work

--- Redis-Example.ps1
+++ Redis-Example.ps1
@@ -2,7 +2,7 @@ Import-Module "$PSScriptRoot\RedisSimple.psm1"
 
 $redisHost = "192.168.0.25"
 $redisPort = 6379
-$totalJobs = 1000
+$totalJobs = 130000
 
 $scriptStart = Get-Date
 
@@ -10,22 +10,23 @@ $scriptStart = Get-Date
 $client = New-Object System.Net.Sockets.TcpClient($redisHost, $redisPort)
 $stream = $client.GetStream()
 
-# Build all commands in one string (pipeline)
-$allCmds = ""
-for ($i = 1; $i -le $totalJobs; $i++) {
-    $key = "key_" + ([guid]::NewGuid().ToString().Substring(0, 5))
-    $value = "value_" + ([guid]::NewGuid().ToString().Substring(0, 8))
-    $allCmds += "*3`r`n`$3`r`nSET`r`n`$" + $key.Length + "`r`n$key`r`n`$" + $value.Length + "`r`n$value`r`n"
+$batchSize = 1000
+for ($offset = 0; $offset -lt $totalJobs; $offset += $batchSize) {
+    $allCmds = ""
+    $end = [math]::Min($offset + $batchSize, $totalJobs)
+    for ($i = $offset + 1; $i -le $end; $i++) {
+        $key = "key_" + ([guid]::NewGuid().ToString().Substring(0, 5))
+        $value = "value_" + ([guid]::NewGuid().ToString().Substring(0, 8))
+        $allCmds += "*3`r`n`$3`r`nSET`r`n`$" + $key.Length + "`r`n$key`r`n`$" + $value.Length + "`r`n$value`r`n"
+    }
+    $cmdBytes = [System.Text.Encoding]::ASCII.GetBytes($allCmds)
+    $stream.Write($cmdBytes, 0, $cmdBytes.Length)
+
+    # Read responses for this batch
+    $buffer = New-Object byte[] ($batchSize * 16)
+    $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
+    # Optionally process $bytesRead/$buffer here
 }
-
-# Send all at once
-$cmdBytes = [System.Text.Encoding]::ASCII.GetBytes($allCmds)
-$stream.Write($cmdBytes, 0, $cmdBytes.Length)
-
-# Read all responses (one per SET)
-$buffer = New-Object byte[] ($totalJobs * 16)
-$bytesRead = $stream.Read($buffer, 0, $buffer.Length)
-$response = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $bytesRead)
 
 $stream.Close()
 $client.Close()
